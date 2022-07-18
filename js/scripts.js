1a385aac9fe483062186fc8c00d6e4da4b90032b
mhammoudoddo <103242844+mhammoudoddo@users.noreply.github.com>
2022-07-18 11:31:00 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
 // Empty JS for your own code to be here
+
+function sendData(){
+  const front = document.getElementById('front');
+  const back = document.getElementById('back');
+
+  var clientId = "mazen.hammoud@ext.oddo-bhf.com";
+  var clientSecret = "KYWIRl5J4QGNY36v4PBT";
+
+// var authorizationBasic = $.base64.btoa(clientId + ':' + clientSecret);
+var authorizationBasic = window.btoa(clientId + ':' + clientSecret);
+
+$.ajax({
+    type: 'POST',
+    url: 'https://integration-api.preventgo.io/v2/any',
+    data: { file_1: '', file_2: 'Smith', options: '{"processImage": true}' },
+    dataType: "json",
+    contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+    // crossDomain: true,
+    headers: {
+       'Authorization': 'Basic ' + authorizationBasic
+    },
+    success: function (result) {
+       $( "#result" ).html( result );
+    },
+    //complete: function (jqXHR, textStatus) {
+    //},
+    error: function (req, status, error) {
+    alert(error);
+    }
+});
+
+}
