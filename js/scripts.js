56293b161d4613f247a81142b049c3f58ca67167
mhammoudoddo <103242844+mhammoudoddo@users.noreply.github.com>
2022-07-18 14:41:09 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,16 @@ function sendData(){
 
 // var authorizationBasic = $.base64.btoa(clientId + ':' + clientSecret);
 var authorizationBasic = window.btoa(clientId + ':' + clientSecret);
+  var logoImg = $('input[name="logoImg"]').get(0).files[0];
+var formData = new FormData();
+formData.append('file_1',  window.URL.createObjectURL(front.files[0]));
+formData.append('file_2',window.URL.createObjectURL(back.files[0]));
+formData.append('options', '{"processImage": true}');
 
 $.ajax({
     type: 'POST',
     url: 'https://integration-api.preventgo.io/v2/any',
-    data: { file_1: front.files[0], file_2: back.files[0], options: '{"processImage": true}' },
+    data: formData,
     dataType: "jsonp",
     contentType: 'application/x-www-form-urlencoded; charset=utf-8',
     crossDomain: true,
