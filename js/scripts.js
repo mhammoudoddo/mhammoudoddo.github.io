18b8dc902dc0935132fc7a7b181d086a60ec72d7
mhammoudoddo <103242844+mhammoudoddo@users.noreply.github.com>
2022-07-18 15:51:45 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,30 @@ function sendData(){
   const front = document.getElementById('front');
   const back = document.getElementById('back');
 
-  var clientId = "mazen.hammoud@ext.oddo-bhf.com";
-  var clientSecret = "KYWIRl5J4QGNY36v4PBT";
-
-// var authorizationBasic = $.base64.btoa(clientId + ':' + clientSecret);
-var authorizationBasic = window.btoa(clientId + ':' + clientSecret);
 var formData = new FormData();
 formData.append('file_1',  window.URL.createObjectURL(front.files[0]));
 formData.append('file_2',window.URL.createObjectURL(back.files[0]));
 formData.append('options', '{"processImage": true}');
 
+
+var myUrl = 'https://integration-api.preventgo.io/v2/any';
+//  But if you make it from a browser, then it will work without problem ...
+
+// However to make it work, we are going to use the cors-anywhere free service to bypass this
+var proxy = 'https://cors-proxy.htmldriven.com/?url=';
+const urlx = 'https://corsproxy.io/?' + encodeURIComponent('https://integration-api.preventgo.io/v2/any');
+
 $.ajax({
     type: 'POST',
-    url: 'https://integration-api.preventgo.io/v2/any',
+    url: urlx,
     data: formData,
-    dataType: "jsonp",
+    dataType: "json",
     contentType: 'application/x-www-form-urlencoded; charset=utf-8',
     crossDomain: true,
     processData: false,
-    xhrFields: { withCredentials: true }, 
+    //xhrFields: { withCredentials: true }, 
     headers: {
-       'Authorization': 'Basic ' + authorizationBasic
+       'Authorization': 'Basic bWF6ZW4uaGFtbW91ZEBleHQub2Rkby1iaGYuY29tOktZV0lSbDVKNFFHTlkzNnY0UEJU'
     },
     success: function (result) {
        $( "#result" ).html( result );
