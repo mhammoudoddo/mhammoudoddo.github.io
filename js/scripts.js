e80e30d5728f8f4e999fadf1d72d56fd28bbb156
mhammoudoddo <103242844+mhammoudoddo@users.noreply.github.com>
2022-07-20 14:27:52 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,64 @@
 // Empty JS for your own code to be here
 
-function sendData(){
-  const front = document.getElementById('front');
+$(document).ready(function() {
+  $('#verify').on('click', function() {
+    var $this = $(this);
+	$this.prop("disabled", true);
+	$('#infomsg').addClass('d-none');
+	$('#errormsg').addClass('d-none');
+    var loadingText = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...';
+    if ($(this).html() !== loadingText) {
+      $this.data('original-text', $(this).html());
+      $this.html(loadingText);
+    }
+	  const front = document.getElementById('front');
   const back = document.getElementById('back');
 
-// var form = new FormData();
-// form.append("file_1", front.files[0], "/C:/Users/mhammoud/Downloads/marine1.png");
-// form.append("file_2", back.files[0], "/C:/Users/mhammoud/Downloads/marine2.png");
-// form.append("options", "{\"processImage\": true}");
-
-
-// const settings = {
-	// "async": true,
-	// "crossDomain": true,
-	// "url": "https://cors-proxy1.p.rapidapi.com/v1",
-	// "method": "POST",
-	// "headers": {
-		// "content-type": "application/json",
-		// "X-RapidAPI-Key": "841f836912msh1a36cce6db2ec3cp17b526jsn645a0be51d83",
-		// "X-RapidAPI-Host": "cors-proxy1.p.rapidapi.com"
-	// },
-	// "processData": false,
-	// "contentType": false,
-	// "mimeType": "multipart/form-data",
-	// "data": {
-		// "url": "https://integration-api.preventgo.io/v2/any",
-		// "method": "POST",
-		// "params": {
-			// "processData": "false",
-            // "contentType": "false",
-			// "mimeType": "multipart/form-data"
-			// },
-		// "data": form,
-		// "headers": {
-			// "content-type": "Content-Type: multipart/form-data",
-			// "Authorization": "Basic bWF6ZW4uaGFtbW91ZEBleHQub2Rkby1iaGYuY29tOktZV0lSbDVKNFFHTlkzNnY0UEJU"
-		// },
-		// "cookies": {}
-	// }
-// };
-
-// $.ajax(settings).done(function (response) {
-	// console.log(response);
-// });
-
+const url = 'https://corsproxy.io/?' + encodeURIComponent('https://integration-api.preventgo.io/v2/any');
 var myHeaders = new Headers();
 myHeaders.append("Authorization", "Basic bWF6ZW4uaGFtbW91ZEBleHQub2Rkby1iaGYuY29tOktZV0lSbDVKNFFHTlkzNnY0UEJU");
 myHeaders.append("Cookie", "ROUTEID=.1");
 
 var formdata = new FormData();
-formdata.append("file_1", front.files[0], "/C:/Users/mhammoud/Downloads/marine1.png");
-formdata.append("file_2", back.files[0], "/C:/Users/mhammoud/Downloads/marine2.png");
+formdata.append("file_1", front.files[0], URL.createObjectURL(front.files[0]));
+formdata.append("file_2", back.files[0], URL.createObjectURL(back.files[0]));
 formdata.append("options", "{\"processImage\": true}");
 
 var requestOptions = {
   method: 'POST',
   headers: myHeaders,
   body: formdata,
-  redirect: 'follow',
-  mode: 'no-cors'
+  redirect: 'follow'
 };
 
-fetch("https://integration-api.preventgo.io/v2/any", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
+fetch(url, requestOptions)
+  .then(response => {console.log(response.status); // Will show you the status
+    if (!response.ok) {
+		$('#errormsg').toggleClass('d-none');
+		$this.html($this.data('original-text'));
+		$this.removeAttr('disabled');
+        throw new Error("HTTP status " + response.status);
+		}
+	return response.json();}
+	)
+  .then(result =>  {
+	  var jsonres= result;
+	  var holder = jsonres.identityDocumentInfo.documentDetails.holder;
+	  var mrz = jsonres.identityDocumentInfo.documentDetails.mrz;
+	  //check if error
+	  if(!jsonres.hasOwnProperty("error"))
+	  {
+		  $('#infomsg').removeClass('d-none');
+		  $('#fullname').html(holder.birthName +' '+ holder.firstName);
+		  $('#details').html('Gender : '+ holder.gender +'<br/>'+'Brith Date: '+ holder.birthDate +'<br/>' +'Brith Place: '+ holder.birthPlace +'<br/>' +'Address : '+ holder.rawAddress +'<br/>' );
+		  $('#footer').html(mrz.line1+mrz.line2);
+	  }
+	  else{
+		  $('#errormsg').removeClass('d-none');
+	  }
+	  
+   })
+  .then(() =>{$this.html($this.data('original-text'));$this.removeAttr('disabled');})
   .catch(error => console.log('error', error));
-
-
-
-// var form = new FormData();
-// form.append("file_1", front.files[0], "/C:/Users/mhammoud/Downloads/marine1.png");
-// form.append("file_2", back.files[0], "/C:/Users/mhammoud/Downloads/marine2.png");
-// form.append("options", "{\"processImage\": true}");
-
-// var settings = {
-  // "url": "http://www.whateverorigin.org/get?url=" + encodeURIComponent('https://integration-api.preventgo.io/v2/any') + "&callback=?",
-  // "method": "POST",
-  // "timeout": 0,
-  // "processData": false,
-  // "mimeType": "multipart/form-data",
-  // "contentType": false,
-  // "data": form
-// };
-
-// $.ajax(settings).done(function (response) {
-  // console.log(response);
-// });
-
-}
+  });
+})
